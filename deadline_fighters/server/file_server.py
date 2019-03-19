f6b3c47b61b3df3336bf77e0881a81c6b5877fcf
Sivaranjani Subramanian <k1898603@kcl.ac.uk>
2019-03-19 20:35:17 +0000

    Upload file through server

---
+++ deadline_fighters/server/file_server.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+import boto3 #AWS S3 API in Python
+import simplejson #Interpret incoming JSON
+import json #Interpret incoming JSON
+import os #Get current directory
+
+from shutil import copyfile
+
+from os.path import expanduser
+homeDir = expanduser("~")
+
+s3_resource = boto3.resource('s3');
+bucketName = "deadlinefighters";
+
+def download_All():
+        print("Reached download_All!")
+        #Insert S3 code here
+def upload_file(fileDetails):
+        fileName = fileDetails["fileName"]
+        copyfile(homeDir+"/deadlinefighters/"+fileName,os.getcwd()+"/"+fileName)
+        s3_resource.Object(bucketName, fileName).upload_file(
+    Filename=fileName)
+        os.remove(os.getcwd()+"/"+fileName)
+        return
+
+
+switcher = {
+    "downloadAll": download_All,
+    "uploadFile": upload_file
+}
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
+
+        print "in post method"
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+        self.send_response(200)
+        self.end_headers()
+
+        data = simplejson.loads(self.data_string)
+
+        func = switcher.get(data["operation"], "Invalid operation")
+
+        return  func(data["fileDetails"])
+
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
